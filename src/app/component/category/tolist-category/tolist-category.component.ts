772873305afd64cc039dc03a5410923143093003
Adriano <gian13088@gmail.com>
2023-11-06 13:52:07 -0500

    category

--- src/app/component/category/tolist-category/tolist-category.component.ts
+++ src/app/component/category/tolist-category/tolist-category.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { Category } from 'src/app/model/category';
+import { CategoryService } from 'src/app/service/category.service';
 
 @Component({
   selector: 'app-tolist-category',
@@ -6,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./tolist-category.component.css']
 })
 export class TolistCategoryComponent {
+  dataSource: MatTableDataSource<Category> = new MatTableDataSource();
+  displayedColumns: string[] = ['idCategory', 'nameCategory','detailsCategory'];
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private cS: CategoryService) {}
+
+  ngOnInit(): void {
+    this.cS.list().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    });
+    this.cS.getList().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+  filter(en: any) {
+    this.dataSource.filter = en.target.value.trim();
+  }
 }
