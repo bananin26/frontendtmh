d02c60c733cef702afd77099fd818185b6d66e67
Adriano <gian13088@gmail.com>
2023-11-06 14:01:40 -0500

    tolist-product

--- src/app/component/product/tolist-product/tolist-product.component.ts
+++ src/app/component/product/tolist-product/tolist-product.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { Product } from 'src/app/model/product';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-tolist-product',
   templateUrl: './tolist-product.component.html',
   styleUrls: ['./tolist-product.component.css']
 })
-export class TolistProductComponent {
+export class TolistProductComponent implements OnInit {
+  dataSource: MatTableDataSource<Product> = new MatTableDataSource();
+  displayedColumns: string[] = ['idProduct', 'nameProduct','descriptionProduct','priceProduct','dimensionsProduct'];
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private uS: ProductService) {}
+
+  ngOnInit(): void {
+    this.uS.list().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    });
+    this.uS.getList().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+  filter(en: any) {
+    this.dataSource.filter = en.target.value.trim();
+  }
 }
