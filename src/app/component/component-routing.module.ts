b961add4556ec09353da0127171982a38c91c2e9
bananin26 <144152663+bananin26@users.noreply.github.com>
2023-11-15 10:36:14 -0500

    commit

--- src/app/component/component-routing.module.ts
+++ src/app/component/component-routing.module.ts
@@ -101,14 +101,10 @@ const routes: Routes = [
     path: 'Home',
     component: HomeComponent,
   },
-<<<<<<< HEAD
-
-=======
   {
     path: 'Map',
     component: MapachevereComponent,
   },
->>>>>>> 03beda004c49ea783bed8d470bbd9a6422c89d14
 ];
 
 @NgModule({
