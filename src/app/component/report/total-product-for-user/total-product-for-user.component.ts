a2ab315560ded1944e3085db4fd067e32e6bf72a
Adriano <gian13088@gmail.com>
2023-11-15 15:03:34 -0500

    q

--- src/app/component/report/total-product-for-user/total-product-for-user.component.ts
+++ src/app/component/report/total-product-for-user/total-product-for-user.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChartOptions } from 'chart.js';
+import { ChartType,ChartDataset } from 'chart.js';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-total-product-for-user',
   templateUrl: './total-product-for-user.component.html',
   styleUrls: ['./total-product-for-user.component.css']
 })
-export class TotalProductForUserComponent {
+export class TotalProductForUserComponent implements OnInit{
+  barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  barChartLabels: string[] = [];
+  barChartType: ChartType = 'pie';
+  barChartLegend = true;
+  barChartData: ChartDataset[] = [];
+  constructor(private pS: ProductService) { }
+  ngOnInit(): void {
+    this.pS.getProducts().subscribe((data) => {
+      this.barChartLabels = data.map((item) => item.name);
+      this.barChartData = [
+        {
+          data: data.map(item => item.totalProducts), label: 'Total de viajes',
+          backgroundColor:'rgba(0,0,0,0.79)'
+        }
+      ]
 
+    });
+  }
 }
