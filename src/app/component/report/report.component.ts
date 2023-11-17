857853d42689d37c105e18c7931e42c79d3125d3
bananin26 <144152663+bananin26@users.noreply.github.com>
2023-11-17 08:23:57 -0500

    commit casi final

--- src/app/component/report/report.component.ts
+++ src/app/component/report/report.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { LoginService } from 'src/app/service/login.service';
 
 @Component({
   selector: 'app-report',
@@ -7,6 +8,27 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./report.component.css']
 })
 export class ReportComponent {
-  constructor(public route: ActivatedRoute){}
+  role: string = '';
+
+  constructor(public route: ActivatedRoute,private loginService: LoginService){}
   ngOnInit(): void{}
+  verificar() {
+    this.role = this.loginService.showRole();
+    return this.loginService.verificar();
+  }
+  cerrar() {
+    sessionStorage.clear();
+  }
+  
+  validarRol() {
+    if (
+      this.role == 'ADMIN' ||
+      this.role == 'CUSTOMER' ||
+      this.role == 'TRAVELER'
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
